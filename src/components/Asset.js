f6d18ced24a3953cd7fc202a5f8165c83a9fcb5e
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2024-01-27 17:50:56 -0300

    feat: add assets description and all footer contatcs

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,6 +1,6 @@
 import styles from "./Asset.module.css"
 
-function Asset({ image, link, title }) {
+function Asset({ image, link, title, description }) {
   const handleClick = () => {
     if (link) {
       window.open(link, "_blank")
@@ -8,11 +8,17 @@ function Asset({ image, link, title }) {
   }
 
   return (
-    <div className={styles.container} onClick={handleClick}>
-      <div className={styles.imageContainer}>
-        <img className={styles.image} src={image} alt="Imagem do Site" />
-        <div className={styles.overlay}></div>
-        <h1 className={styles.text}>{title}</h1>
+    <div className={styles.asset}>
+      <div className={styles.description}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.aboutAsset}>{description}</p>
+      </div>
+      <div className={styles.container} onClick={handleClick}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={image} alt="Imagem do Site" />
+          <div className={styles.overlay}></div>
+          <h1 className={styles.text}>{title}</h1>
+        </div>
       </div>
     </div>
   )
--- src/pages/Institutional.js
+++ src/pages/Institutional.js
@@ -53,35 +53,86 @@ function Institutional() {
       </div>
 
       <div className="assets">
-        <h1>
-          <strong>Nosso Portfólio</strong>
-        </h1>
+        <div className="description">
+          <h1>
+            <strong>Nosso Portfólio</strong>
+          </h1>
+          <p>
+            Nós desenvolvemos sistemas de gestão, automação, plataformas, sites,
+            aplicativos e o que seu negócio precisar!
+            <br />
+            Confira nossos principais projetos abaixo.
+          </p>
+        </div>
         <div className="cards">
           <Asset
             image={nakadocinhos}
             title={"Nakadocinhos"}
+            description={
+              "Um cardápio digital para os deliciosos doces e bolos personalizados do Nakadocinhos, facilitando a divulgação da marca e o primeiro contato com o cliente."
+            }
             link={"https://www.nakadocinhos.com.br"}
           ></Asset>
           <Asset
             image={sextaholdem}
             title={"Sexta Hold'em"}
+            description={
+              "Uma plataforma online para um Home Game de Poker, com avisos, rankings, estapas, fotos, permitindo a divulgação do local. O Administrador da plataforma pode gerenciar todos os dados, facilitando a gestão."
+            }
             link={"https://www.sextaholdem.com.br"}
           ></Asset>
         </div>
       </div>
 
       <footer>
-        <p>Contato</p>
+        <p>
+          <strong>Entre em contato conosco!</strong>
+        </p>
         <div className="contact">
-          <SocialMedia
-            image={linkedin}
-            link={"https://www.linkedin.com/in/lucas-nakagawa/"}
-          ></SocialMedia>
-          <SocialMedia
-            image={whatsapp}
-            link={"https://wa.me/5511998877717"}
-          ></SocialMedia>
-          <SocialMedia image={gmail}></SocialMedia>
+          <div className="cellphones">
+            <div className="item">
+              <SocialMedia
+                image={whatsapp}
+                link={"https://wa.me/5511998877717"}
+              ></SocialMedia>
+              <p>(11) 99887-7717</p>
+              <p>Lucas Nakagawa</p>
+            </div>
+
+            <div className="item">
+              <SocialMedia
+                image={whatsapp}
+                link={"https://wa.me/5511988742687"}
+              ></SocialMedia>
+              <p>(11) 98874-2687</p>
+              <p>Mateus Fugita</p>
+            </div>
+
+            <div className="item">
+              <SocialMedia
+                image={whatsapp}
+                link={"https://wa.me/5511967535340"}
+              ></SocialMedia>
+              <p>(11) 96753-5340</p>
+              <p>João Paulo</p>
+            </div>
+          </div>
+          <div className="socialMedias">
+            <div className="item">
+              <SocialMedia
+                image={linkedin}
+                link={"https://www.linkedin.com/in/lucas-nakagawa/"}
+              ></SocialMedia>
+              <p>linkedin.com/in/lucas-nakagawa/</p>
+            </div>
+            <div className="item">
+              <SocialMedia image={gmail}></SocialMedia>
+              <p>najf@gmail.com</p>
+            </div>
+            <h3 className="copyright">
+              © DESENVOLVIDO POR NAJF TECH SOLUTIONS
+            </h3>
+          </div>
         </div>
       </footer>
     </div>
